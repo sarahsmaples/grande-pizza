43682db2324d7e4d956607f81a316d3a68d9f61b
Alex <tripscape+github@protonmail.com>
2025-09-10 13:59:30 -0500

    Init

---
+++ src/_data/client.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: 'Client Name',
+  address: {
+    city: 'City',
+    state: 'State',
+    zip: '12345',
+  },
+  phone: '123-456-7890',
+  domain: 'https://example.com',
+  id: '123',
+}
