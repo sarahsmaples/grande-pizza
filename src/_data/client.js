6c524a6a6680a4c171b2f98c9192c95a4a98f82b
Alex <tripscape+github@protonmail.com>
2025-09-11 12:32:31 -0500

    Use proper urls

--- src/_data/client.js
+++ src/_data/client.js
@@ -50,11 +50,6 @@ module.exports = {
   googleMapsEmbedUrl:
     'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3345.8400021938537!2d-97.0494780872197!3d33.007993472033455!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x864c2c56f4bec7a7%3A0xa6d2a1b3c5c33796!2s1111%20Lexington%20Ave%2C%20Flower%20Mound%2C%20TX%2075028!5e0!3m2!1sen!2sus!4v1757601973305!5m2!1sen!2sus',
 
-  // Online Services
-  reservationUrl: 'https://testrestaurant.com/reservations',
-  orderUrl: 'https://testrestaurant.com/order',
-  contactFormUrl: 'https://88restaurants.com/1119/contact_forms',
-  widgetUrl: 'https://88restaurants.com/1119/widgets/5129',
 
   // System Integration IDs
   id: '1119',
